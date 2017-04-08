a147cd3ed01f18beb60194f367a71ace3f55f8d2
Egarat Kaoroprat <egarat.kaoroprat@gmail.com>
2017-04-08 23:15:32 +0200

    Add geolocation support

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-const {generateMessage} = require('./message');
+const {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -15,4 +15,19 @@ describe('generateMessage', () => {
     expect(message.createdAt).toBeA('number');
   });
 
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    var from = 'Egarat';
+    var latitude = '1337';
+    var longitude = '7331';
+    var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+    expect(locationMessage).toInclude({
+      from,
+      url: `https://www.google.com/maps?q=${latitude},${longitude}`
+    });
+    expect(locationMessage.createdAt).toBeA('number');
+  });
 });
